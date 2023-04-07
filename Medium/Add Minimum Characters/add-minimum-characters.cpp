da6fb1593839e1d973b4ad6e28b91025e12c693d
Reva Tamaskar <revatamaskar22@gmail.com>
2023-04-07 18:26:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add Minimum Characters/add-minimum-characters.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int addMinChar(string str){    
+        //code here
+        int n = str.length();
+        int ans = n-1,i=0,j=n-1,st=i;
+        
+        while(i<=j){
+            if(str[i]==str[j]){
+                i++;j--;
+            }
+            else{
+                ans--;
+                i=0;j=ans;
+            }
+        }
+        return n-1-ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+        
+        Solution ob;
+        cout << ob.addMinChar(str) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
